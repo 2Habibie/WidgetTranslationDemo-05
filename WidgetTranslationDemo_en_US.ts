ed143e4a2de7f7748c65ab1041d9ca2215797bf3
2Habibie <GitHUB.Habibie@recursor.net>
2021-10-05 18:24:24 -0400

    Preliminary commit

---
+++ WidgetTranslationDemo_en_US.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>CustomWidget</name>
+    <message>
+        <location filename="customwidget.ui" line="14"/>
+        <source>Widget Translation Demo</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="customwidget.ui" line="98"/>
+        <source>Select language</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="customwidget.ui" line="112"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="customwidget.ui" line="117"/>
+        <source>German</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="customwidget.ui" line="122"/>
+        <source>Spanish</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="customwidget.ui" line="42"/>
+        <source>Welcome!</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
